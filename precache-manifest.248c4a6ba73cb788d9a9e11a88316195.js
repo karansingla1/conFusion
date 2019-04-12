8efdce14e1f992448ae2a5f6725e39920d277987
System Administrator <singlakaran1994@gmail.com>
2019-04-12 10:47:26 +0530

    Updates

---
+++ precache-manifest.248c4a6ba73cb788d9a9e11a88316195.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "157058a33cfe981df7f1",
+    "url": "/conFusion/static/css/main.9949f423.chunk.css"
+  },
+  {
+    "revision": "157058a33cfe981df7f1",
+    "url": "/conFusion/static/js/main.7cfc13b8.chunk.js"
+  },
+  {
+    "revision": "d75f40fedbbda6a7d2ca",
+    "url": "/conFusion/static/js/runtime~main.cac4731c.js"
+  },
+  {
+    "revision": "00b24eae9248e7edeff3",
+    "url": "/conFusion/static/css/2.5a53a313.chunk.css"
+  },
+  {
+    "revision": "00b24eae9248e7edeff3",
+    "url": "/conFusion/static/js/2.b36f5ff1.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/conFusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/conFusion/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/conFusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/conFusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/conFusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2f5adc1a19c29124c2ee732b8ab351c2",
+    "url": "/conFusion/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b675d064d65ee70ea73020dc75450af.js"
+  "/conFusion/precache-manifest.248c4a6ba73cb788d9a9e11a88316195.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/conFusion/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
